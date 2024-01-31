860cf6c60c51230a52aa42fba2ce00d5b6fef83a
Jean Carlos De Meira <jean.meira10@hotmail.com>
2024-01-30 22:03:20 -0300

    Feat: add crud for one ong

---
+++ Controllers/OngsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngResgisterApi.Models;
+using RestaurantApi.Services;
+
+namespace OngResgisterApi.Controllers
+{
+    [Route("/api/ongs")]
+    [ApiController]
+    public class OngsController : Controller
+    {
+        private OngsService _ongsService;
+
+        public OngsController(OngsService service) => _ongsService = service;
+
+        [HttpGet]
+        public async Task<List<Ong>> Get() => await _ongsService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Ong>> Get(string id)
+        {
+            var ong = await _ongsService.GetAsync(id);
+            if(ong == null) return NotFound();
+            return Ok(ong);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Ong newOng)
+        {
+            await _ongsService.CreateAsync(newOng);
+            return CreatedAtAction(nameof(Get), new {id= newOng.Id}, newOng);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Ong updatedOng)
+        {
+            var ong = await _ongsService.GetAsync(id);
+            if(ong == null) return NotFound();
+
+            await _ongsService.UpdateAsync(id, updatedOng);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var ong = await _ongsService.GetAsync(id);
+            if(ong == null) return NotFound();
+            await _ongsService.RemoveAsync(id);
+            return NoContent();
+        }
+    }
+}
