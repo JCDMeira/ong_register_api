e873bda7209e8eef087b1b4eae703ba2cac80946
Jean Carlos De Meira <jean.meira10@hotmail.com>
2024-01-31 21:52:37 -0300

    Feat: add new fields on Ongs model

--- Models/Ong.cs
+++ Models/Ong.cs
@@ -4,5 +4,11 @@
     {
     public string Name { get; set; } = null!;
     public string? Description { get; set; } = null!;
+
+    public string? ImageUrl { get; set; } = null!;
+
+    public string Purpose { get; set; } = null!;
+
+    public string HowToAssist { get;  set; } = null!;
 }
 
